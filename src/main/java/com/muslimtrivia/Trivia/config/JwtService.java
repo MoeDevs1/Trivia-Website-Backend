0e3eec30c2ff1a407f6b492ca8ded64973dccc75
moeymoe111 <mohannadosman56@gmail.com>
2023-05-30 10:52:32 -0400

    First commit

---
+++ src/main/java/com/muslimtrivia/Trivia/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.muslimtrivia.Trivia.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUserName(String jwtToken) {
+        // Implementation goes here...
+        return null;
+    }
+}
---
+++ src/main/java/com/muslimtrivia/Trivia/user/User.java
@@ -0,0 +1,68 @@
+package com.muslimtrivia.Trivia.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Integer Id;
+    private String lastName;
+    private String firstName;
+    private String email;
+    private String password;
+    private String userName;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
